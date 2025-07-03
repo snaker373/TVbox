68132d37756c114aab4955031d219de35ae3735b
German <ryndagerman@gmail.com>
2025-07-03 17:54:17 +0300

    add a Box 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         var textBox1 = new TextBox();
         textBox1.setText("Box 1");
         System.out.println(textBox1.text);
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
     }
 }
