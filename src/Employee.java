3e1a35497ffaee60f3fee9d3986d155c7abc54d0
German <ryndagerman@gmail.com>
2025-07-03 20:31:25 +0300

    creating_an_Error

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,15 @@
 public class Employee {
-    public int baseSalary;
+    private int baseSalary;
     public int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + (extraHours * hourlyRate);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Base salary must be greater than zero");
+        }
+        this.baseSalary = baseSalary;
     }
 }
